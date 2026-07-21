b50cdcfd243727973a59cbf3edd13f6386b5222e
YannikBorbor <saulborbor56@gmail.com>
2026-07-20 20:48:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,53 @@ formulario.addEventListener("submit", function(e){
 
     formulario.reset();
 });
+
+const formOpinion = document.getElementById("formOpinion");
+
+formOpinion.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const nombre = document.getElementById("nombreCliente").value;
+
+    const estrellas = document.getElementById("calificacion").value;
+
+    const comentario = document.getElementById("comentario").value;
+
+    const nuevaOpinion = document.createElement("div");
+
+    nuevaOpinion.className = "opinion";
+
+    nuevaOpinion.innerHTML = `
+        <h4>${estrellas} ${nombre}</h4>
+        <p>${comentario}</p>
+    `;
+
+    document.getElementById("listaOpiniones").prepend(nuevaOpinion);
+
+    formOpinion.reset();
+
+    alert("🎉 ¡Gracias por compartir tu opinión!");
+
+});
+
+const btnArriba = document.getElementById("btnArriba");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        btnArriba.style.display = "block";
+    }else{
+        btnArriba.style.display = "none";
+    }
+
+});
+
+btnArriba.addEventListener("click", () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
